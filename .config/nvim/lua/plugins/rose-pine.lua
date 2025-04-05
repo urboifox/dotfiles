9f702b63faabb2e50d78fa874c4382ca8f00fd09
Fox <urboifox@gmail.com>
2025-04-05 20:26:39 +0200

    dual monitor with bspwm

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -16,8 +16,61 @@ return {
             },
 
             highlight_groups = {
-                -- CursorLine = { bg = '#222222' },
                 CursorLine = { bg = 'None' },
+                -- StatusLine = { fg = "love", bg = "muted", blend = 15 },
+                VertSplit = { fg = "muted", bg = "muted" },
+                Visual = { fg = "base", bg = "text", inherit = false },
+            },
+
+            groups = {
+                border = 'muted',
+                link = 'iris',
+                panel = 'surface',
+
+                error = 'love',
+                hint = 'iris',
+                info = 'foam',
+                note = 'pine',
+                todo = 'rose',
+                warn = 'gold',
+
+                git_add = 'foam',
+                git_change = 'rose',
+                git_delete = 'love',
+                git_dirty = 'rose',
+                git_ignore = 'muted',
+                git_merge = 'iris',
+                git_rename = 'pine',
+                git_stage = 'iris',
+                git_text = 'rose',
+                git_untracked = 'subtle',
+
+                h1 = 'iris',
+                h2 = 'foam',
+                h3 = 'rose',
+                h4 = 'gold',
+                h5 = 'pine',
+                h6 = 'foam',
+            },
+
+            before_highlight = function(group, highlight, palette)
+                -- Disable all undercurls
+                -- if highlight.undercurl then
+                --     highlight.undercurl = false
+                -- end
+                --
+                -- Change palette colour
+                -- if highlight.fg == palette.pine then
+                --     highlight.fg = palette.foam
+                -- end
+            end,
+
+            palette = {
+                -- Override the builtin palette per variant
+                -- moon = {
+                --     base = '#18191a',
+                --     overlay = '#363738',
+                -- },
             },
 
             styles = {
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+killall -q polybar
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload toph &
