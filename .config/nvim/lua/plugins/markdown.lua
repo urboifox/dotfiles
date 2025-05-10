c0310a6f743c001ebcae9eff2958a8cb3e4bedd6
Fox <urboifox@gmail.com>
2025-05-10 14:25:28 +0300

    add markdown renderer

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,24 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        preset = 'none', -- Avoids default styles that may include backgrounds
+        code = {
+            disable_background = true,
+            highlight_border = false,
+            style = 'language',
+        },
+        heading = {
+            backgrounds = { 'NONE' },
+        },
+        inline_code = {
+            hl_group = 'NONE',
+        },
+    },
+    ft = 'markdown',
+    config = function(_, opts)
+        require('render-markdown').setup(opts)
+    end,
+}
