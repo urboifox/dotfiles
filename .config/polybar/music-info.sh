1f4dfd1c45fc06bc21e0f48f9a22cbde269670db
Fox <urboifox@gmail.com>
2025-05-15 15:00:37 +0300

    replace artist with title

--- .config/polybar/music-info.sh
+++ .config/polybar/music-info.sh
@@ -3,7 +3,7 @@
 player_status="$(playerctl status 2> /dev/null)"
 
 if [[ "$player_status" = "Playing" || "$player_status" = "Paused" ]]; then
-    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "$(playerctl metadata title) - $(playerctl metadata artist)"
 else
     echo ""
 fi
