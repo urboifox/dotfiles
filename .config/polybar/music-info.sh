7aad5a5f92ccef2959eccc8d535d185b20d4a2dc
Fox <urboifox@gmail.com>
2025-05-15 15:00:09 +0300

    fix music info script

--- .config/polybar/music-info.sh
+++ .config/polybar/music-info.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 player_status="$(playerctl status 2> /dev/null)"
 
 if [[ "$player_status" = "Playing" || "$player_status" = "Paused" ]]; then
-    echo "$(playerctl metadata xesam:artist) - $(playerctl metadata xesam:title)"
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 else
     echo ""
 fi
