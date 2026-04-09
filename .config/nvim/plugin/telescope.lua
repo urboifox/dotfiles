ba5e200f908327d1e2e4473df04541b0b7266e34
Fox <urboifox@gmail.com>
2026-04-10 00:35:28 +0200

    add code actions to telescope

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -10,8 +10,14 @@ require("telescope").setup({
 })
 
 local builtin = require("telescope.builtin")
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Search files" })
 vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Search for string in files" })
 vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search diagnostics" })
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Search Recent Files" })
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
+
+vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Goto definition" })
+vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Goto references" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
