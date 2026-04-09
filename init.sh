3936c1656c217c77ba05183faba3524ae69348c3
Fox <urboifox@gmail.com>
2026-04-10 00:05:12 +0200

    add fonts installation

--- init.sh
+++ init.sh
@@ -68,4 +68,13 @@ if [ "$SHELL" != "$(which zsh)" ]; then
     chsh -s "$(which zsh)"
 fi
 
+# fonts
+sudo apt install fontconfig
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+mkdir -p $HOME/.local/share/fonts
+unzip Meslo.zip -d $HOME/.local/share/fonts
+rm $HOME/.local/share/fonts/*Windows*
+rm Meslo.zip
+fc-cache -fv
+
 echo "Installation finished! Reboot or log out to see changes."
