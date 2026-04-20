6941c21ebf9513c5b4ee8dcb3c2500f531561740
Fox <urboifox@gmail.com>
2026-04-20 13:56:39 +0200

    start treesitter automatically on specific files

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,7 +1,7 @@
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
@@ -10,38 +10,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Remove quick fix item when pressing dd
 local function remove_qf_item()
     local qf_list = vim.fn.getqflist()
-    local curqfidx = vim.fn.line '.'
+    local curqfidx = vim.fn.line(".")
     table.remove(qf_list, curqfidx)
-    vim.fn.setqflist(qf_list, 'r')
-    vim.cmd((curqfidx + 1) .. 'cfirst')
-    vim.cmd 'copen'
+    vim.fn.setqflist(qf_list, "r")
+    vim.cmd((curqfidx + 1) .. "cfirst")
+    vim.cmd("copen")
 end
 
-vim.api.nvim_create_user_command('RemoveQFItem', remove_qf_item, {})
+vim.api.nvim_create_user_command("RemoveQFItem", remove_qf_item, {})
 
--- Neovim ships ftplugin for svelte but no syntax/svelte.vim and no vim.treesitter.start()
--- (unlike e.g. lua). Start Tree-sitter here so highlighting matches ensure_installed.
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'svelte',
-    callback = function(ev)
-        local buf = ev.buf
-        vim.treesitter.start(buf)
-        vim.b[buf].undo_ftplugin = (vim.b[buf].undo_ftplugin or '')
-            .. ' | lua vim.treesitter.stop()'
-    end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'qf',
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "qf",
     callback = function()
-        vim.keymap.set('n', 'dd', ':RemoveQFItem<CR>', { buffer = true, silent = true })
+        vim.keymap.set("n", "dd", ":RemoveQFItem<CR>", { buffer = true, silent = true })
     end,
 })
 
 -- prevent commenting the next line to a comment
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = '*',
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
     callback = function()
-        vim.opt_local.formatoptions:remove { 'r', 'o' }
+        vim.opt_local.formatoptions:remove({ "r", "o" })
+    end,
+})
+
+local treesitter_autostart = vim.api.nvim_create_augroup("treesitter-autostart", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = treesitter_autostart,
+    pattern = { "svelte", "typescriptreact", "javascriptreact", "css", "scss", "less" },
+    callback = function(ev)
+        local buf = ev.buf
+        pcall(vim.treesitter.start, buf)
+        vim.b[buf].undo_ftplugin = (vim.b[buf].undo_ftplugin or "") .. " | lua pcall(vim.treesitter.stop)"
     end,
 })
