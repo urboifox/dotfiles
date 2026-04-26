cefdb7415a17b913dc2afc75c443b705c370a9e0
Fox <urboifox@gmail.com>
2026-04-26 17:38:48 +0300

    feat: add a keymap to export kulala collection

--- .config/nvim/plugin/kulala.lua
+++ .config/nvim/plugin/kulala.lua
@@ -13,6 +13,20 @@ vim.api.nvim_create_autocmd("FileType", {
             lsp = { formatter = true },
             ui = { display_mode = "split" },
         })
+
+        vim.keymap.set("n", "<leader>ke", function()
+            vim.ui.input({
+                prompt = "Kulala export path: ",
+                default = vim.fn.getcwd(),
+            }, function(input)
+                if not input or input == "" then
+                    return
+                end
+
+                require("kulala").export(input)
+            end)
+        end, { buffer = true, desc = "Export Kulala request" })
+
         vim.treesitter.start()
     end,
 })
